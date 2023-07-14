17e4d134e8af767e03e42d827a159dd3bd7c64f5
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 20:57:57 +0530

    re-intialiazed viewmodel variable

--- app/src/main/java/com/example/forage/ui/ForageableListFragment.kt
+++ app/src/main/java/com/example/forage/ui/ForageableListFragment.kt
@@ -27,6 +27,7 @@ import com.example.forage.R
 import com.example.forage.databinding.FragmentForageableListBinding
 import com.example.forage.ui.adapter.ForageableListAdapter
 import com.example.forage.ui.viewmodel.ForageableViewModel
+import com.example.forage.ui.viewmodel.ForageableViewModelFactory
 
 /**
  * A fragment to view the list of [Forageable]s stored in the database.
@@ -40,7 +41,7 @@ class ForageableListFragment : Fragment() {
     //  from BaseApplication
     private val viewModel: ForageableViewModel by activityViewModels {
         ForageableViewModelFactory(
-            (activity?.application as BaseApplication).database.foragableDao()
+            (activity?.application as BaseApplication).database.forageDao()
         )
     }
 
